522962429064e9f068dc131b93237b02a1966db6
Thiago Moraes <thiago_mc22@hotmail.com>
2025-09-11 22:04:45 -0300

    Adicao de rotas para gerar a ficha por IA

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FichaTreinoController;
+use App\Infrastructure\Http\Controllers\FichaTreinoController as DDDFichaTreinoController;
+use App\Http\Controllers\FichaTreinoController as HttpFichaTreinoController;
+use App\Infrastructure\Http\Controllers\AlunoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/fichas-treino', [FichaTreinoController::class, 'store']);
+Route::post('/fichas-treino', [HttpFichaTreinoController::class, 'store']);
+Route::post('/fichas/suggest', [DDDFichaTreinoController::class, 'suggest']);
+Route::resource('/alunos', AlunoController::class);
